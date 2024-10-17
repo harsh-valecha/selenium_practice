bbb920fa8bbb3e201338c0747b5038f4b4bb0091
Harsh <harsh.valecha@yahoo.com>
2024-10-17 08:25:25 +0530

    Initial commit

---
+++ tests/test_17102024.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+'''
+driver.get() - navigate to url
+driver.title - fetches title 
+driver.page_source
+browser options
+    -
+'''
+
+def test_task2():
+    driver = webdriver.Chrome()
+    driver.get('https://www.google.com/')
+    page_source = driver.page_source
+    assert 'google' in page_source
+    driver.quit()
+
+
+def test_chrome_options():
+    chrome_options = Options()
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('--start-maximized')
+
+    driver = webdriver.Chrome(chrome_options)
+    driver.quit()
+
+
